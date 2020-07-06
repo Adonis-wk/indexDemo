1be6353cc7f9b44bbfa1e8b8dd71e4502a8fec05
wangkai33 <!Adonis6Wk>
2020-07-06 11:53:33 +0800

    修改data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,10 +2,6 @@ export const getUserInfo =
 {
   'success': true,
   'body': {
-    'sellerName': '11',
-    'sellerNo': 'ECP0000000002923',
-    'tenantId': 1,
-    'userPin': 'wangkai33',
-    'userType': '0'
+    'userPin': 'xiaoMing'
   }
 }
